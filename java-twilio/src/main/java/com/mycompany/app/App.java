feca188da5fc35ad661b2e56ae56dd1e2fcb0cd3
devharp <hsingh.161297@gmail.com>
2023-06-28 13:34:18 +0530

    Added java-twilio project

--- java-twilio/src/main/java/com/mycompany/app/App.java
+++ java-twilio/src/main/java/com/mycompany/app/App.java
@@ -3,19 +3,19 @@ package com.mycompany.app;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 
-/**
- * Hello world!
- *
- */
+ /**
+  * The App class is the entry point for the Twilio messaging application.
+  * It sends a WhatsApp message using the Twilio API.
+  */
 public class App {
-    public static final String ACCOUNT_SID = "AC87d268fc8702274f083f6c42c58769ce";
-    public static final String AUTH_TOKEN = "3850bb7071a3170de90465fceeb137e6";
+    public static final String ACCOUNT_SID = "AC87d268fc8702274f083f6c42c58769ce";          // Twilio Account SID used for authentication
+    public static final String AUTH_TOKEN = "8dbce5a2c5e236208265935f526e3d37";             // Twilio Auth Token used for authentication
     public static void main( String[] args ) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
-                new com.twilio.type.PhoneNumber("whatsapp:+917796591414"),
-                new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-                "Hello there!, msg from TWILIO")
+                new com.twilio.type.PhoneNumber("whatsapp:+917020876827"),                  // Receivers phone number
+                new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),                   // Senders phone number ie Twilio's number
+                "Hello there harp!, msg from TWILIO")
             .create();
 
         System.out.println(message.getSid());
