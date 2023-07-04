4e9ff83d1a685963ebc3df92bea4772d3025077d
devharp <hsingh.161297@gmail.com>
2023-07-04 18:16:58 +0530

    Cloudinary integrated

--- java-cloudinary/src/main/java/com/harp/JavaCloudinaryApplication.java
+++ java-cloudinary/src/main/java/com/harp/JavaCloudinaryApplication.java
@@ -7,15 +7,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StreamUtils;
@@ -26,9 +23,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @SpringBootApplication
 @RestController
 public class JavaCloudinaryApplication {
+
+	private ImageUploader imageUploader;	
 
 	@RequestMapping("/")
 	public String home() {
@@ -38,6 +40,7 @@ public class JavaCloudinaryApplication {
 
 	@GetMapping("/file-upload")
 	public ResponseEntity<String> fileUploadForm() {
+
 		try {
 			ClassPathResource resource = new ClassPathResource("public/file-upload.html");
 			String htmlContent = StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
@@ -55,23 +58,29 @@ public class JavaCloudinaryApplication {
 	}
 
 	@PostMapping("/upload")
-    public ResponseEntity<String> uploadFile(@RequestParam("files") MultipartFile[] files) {
-				
+    public ResponseEntity<String> uploadFile(@RequestParam("files") MultipartFile[] files) throws JsonProcessingException {
+			List<String> filesPath = new ArrayList<String>();
+			ObjectMapper objectMapper = new ObjectMapper();
+			imageUploader = new ImageUploader("duiloptvq", "586615437941542", "5HSuzKJGHh_v4SIOQZy_c9HU8-0");
+			
 			Arrays.asList(files).forEach(file -> {
 					String fileName = file.getOriginalFilename();
 					Path filePath = Paths.get(System.getProperty("user.dir"), "uploads", fileName);
         	File destinationFile = filePath.toFile();
 
 					try {
-						if (!destinationFile.exists() && destinationFile.createNewFile()) {
+						if(destinationFile.exists()) destinationFile.delete();
+						if (destinationFile.createNewFile()) {
 								file.transferTo(destinationFile);
 								System.out.println("File transferred: " + fileName);
+								filesPath.add(imageUploader.uploadImage(new File(filePath.toAbsolutePath().toString())));
 							}
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
 			});
-			return ResponseEntity.status(HttpStatus.OK).body("Files Received");
+			System.out.println("FILES PATH: " + filesPath);
+			return ResponseEntity.status(HttpStatus.OK).body(objectMapper.writeValueAsString(filesPath));
 			
     }
 
